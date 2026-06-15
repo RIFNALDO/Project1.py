5d08826e58c4e1efb7a779a35eac60b170988b35
RIFNALDO <rumana3712@gmail.com>
2026-06-15 18:27:23 -0400

    Add files via upload

---
+++ Project1.py
@@ -0,0 +1,10 @@
+# Morning 
+print(" Wake up and brush your teeth")
+print(" Eat breakfast and study")
+# Noon 
+print("Play outside and eat lunch ")
+# Afternoon 
+print(" Read and study or practice soccer")
+# Night 
+print("Play video games then sleep")
+print (" Total study time is 2*2=",2*2)
